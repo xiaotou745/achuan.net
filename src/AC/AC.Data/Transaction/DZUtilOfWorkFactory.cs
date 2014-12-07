819c5e381db4897ae384b40af87c6a7cd53ba499
sylar.wang@foxmail.com <sylar.wang@foxmail.com>
2014-12-07 17:17:08 +0800

    add ac.data project
    add ac.data project

---
+++ src/AC/AC.Data/Transaction/DZUtilOfWorkFactory.cs
@@ -0,0 +1,27 @@
+﻿using AC.Data.ConnString.Common;
+using AC.Transaction.Common;
+
+namespace AC.Transaction
+{
+    public class DZUtilOfWorkFactory
+    {
+        public static IUnitOfWork GetUnitOfWorkOfEAdmin()
+        {
+            return UnitOfWorkFactory.GetAdoNetUnitOfWork(ConnStringUtil.GetConnectionString("dz_d_planEntities"));
+        }
+
+        public static IUnitOfWork GetUnitOfWorkOfBuy()
+        {
+            return UnitOfWorkFactory.GetAdoNetUnitOfWork(ConnStringUtil.GetConnectionString("ConnStringOfDZ"));
+        }
+
+        public static IUnitOfWork GetUnitOfWordOfDZ()
+        {
+            return UnitOfWorkFactory.GetAdoNetUnitOfWork(ConnStringUtil.GetConnectionString("dz"));
+        }
+        public static IUnitOfWork GetUnitOfWordOfDZNew()
+        {
+            return UnitOfWorkFactory.GetAdoNetUnitOfWork(ConnStringUtil.GetConnectionString("ConnStringOfDZ"));
+        }
+    }
+}
