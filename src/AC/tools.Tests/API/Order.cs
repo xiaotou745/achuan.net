035fee7798c35e214b30489f0bfc286c5d5dd43c
wangyuchuan <wang.yuchuan@etaoshi.com>
2015-03-11 21:17:57 +0800

    add test files

---
+++ src/AC/tools.Tests/API/Order.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tools.Tests.API
+{
+    public class ResponseResult
+    {
+        public string code { get; set; }
+        public object response { get; set; }
+    }
+    
+
+    public class ApiParams
+    {
+        public string app_key { get; set; }
+        public string timestamp { get; set; }
+        public string v { get; set; }
+        public string sign { get; set; }
+        public object fields { get; set; }
+    }
+
+    public class OrderCreateParam
+    {
+        
+    }
+
+    public class StoreInfo
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string 
+    }
+
+    public class OrderStatusGetParam
+    {
+        public string order_id { get; set; }
+    }
+
+    public class OrderStatusGetResult
+    {
+        public string status { get; set; }
+    }
+}
---
+++ src/AC/tools.Tests/Test/OrderTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using AC.Util;
+using Common.Logging;
+using NUnit.Framework;
+using tools.Tests.API;
+
+namespace tools.Tests.Test
+{
+    [TestFixture]
+    public class OrderTest
+    {
+        private readonly ILog logger = LogManager.GetCurrentClassLogger();
+
+        [Test]
+        public void OrderStatusGetResultTest()
+        {
+            ResponseResult result = new ResponseResult();
+            result.code = "0";
+            result.response = new OrderStatusGetResult()
+                {
+                    status = "1"
+                };
+           
+            string jsonResult = JsonHelper.ToJson(result);
+            logger.Info(jsonResult);
+        }
+
+        [Test]
+        public void OrderStatusGetParamTest()
+        {
+            OrderStatusGetParam param = new OrderStatusGetParam();
+            param.order_id = "12121212121";
+            string jsonResult = JsonHelper.ToJson(param);
+            //logger.Info(jsonResult);
+
+            ApiParams parameter = new ApiParams();
+
+            parameter.app_key = "wanda";
+            parameter.v = "1.0";
+            parameter.timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            parameter.sign = "D58E3582AFA99040E27B92B13C8F2280";
+            parameter.fields = param;
+
+            logger.Info(JsonHelper.ToJson(parameter));
+        }
+    }
+}
