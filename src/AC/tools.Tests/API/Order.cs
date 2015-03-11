e87508ca4fcc72a749d40a0fecf8a08ce6f3df34
achuan <sylar.wang@foxmail.com>
2015-03-12 00:33:25 +0800

    add some test files

--- src/AC/tools.Tests/API/Order.cs
+++ src/AC/tools.Tests/API/Order.cs
@@ -24,14 +24,61 @@ namespace tools.Tests.API
 
     public class OrderCreateParam
     {
-        
+        public string order_id { get; set; }
+        public DateTime create_time { get; set; }
+        public DateTime receive_time { get; set; }
+        public bool is_pay { get; set; }
+        public double total_price { get; set; }
+        public AddressInfo address { get; set; }
+        public StoreInfo store_info { get; set; }
+        public OrderDetailInfo[] order_details { get; set; }
+        public string remark { get; set; }
+        public double weight { get; set; }
+        public double delivery_fee { get; set; }
+        public int package_count { get; set; }
+    }
+
+    public class OrderCreateResponse
+    {
+        public string order_id { get; set; }
     }
 
     public class StoreInfo
     {
-        public string name { get; set; }
-        public string id { get; set; }
-        public string 
+        public string store_id { get; set; }
+        public string store_name { get; set; }
+        public string group{get;set;}
+        public string id_card { get; set; }
+        public string phone { get; set; }
+        public string phone2 { get; set; }
+        public string province_code { get; set; }
+        public string city_code { get; set; }
+        public string area_code { get; set; }
+        public string address { get; set; }
+        public double longitude { get; set; }
+        public double latitude { get; set; }
+        public double delivery_fee { get; set; }
+        public string commission_type { get; set; }
+    }
+
+    public class AddressInfo
+    {
+        public string user_name { get; set; }
+        public string user_phone { get; set; }
+        public string province_code { get; set; }
+        public string city_code { get; set; }
+        public string area_code { get; set; }
+        public string address { get; set; }
+        public double longitude { get; set; }
+        public double latitude { get; set; }
+    }
+
+    public class OrderDetailInfo
+    {
+        public long detail_id { get; set; }
+        public string product_name { get; set; }
+        public double unit_price { get; set; }
+        public int quantity { get; set; }
     }
 
     public class OrderStatusGetParam
